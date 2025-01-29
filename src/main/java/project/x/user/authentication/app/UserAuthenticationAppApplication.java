1f67c64596f0dd073b41a69c1b350eecdd3e1640
gowrishsbhandary <gowrishsbhandary@gmail.com>
2025-01-29 16:32:28 +0200

    Spring boot App setup

---
+++ src/main/java/project/x/user/authentication/app/UserAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package project.x.user.authentication.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationAppApplication.class, args);
+	}
+
+}
